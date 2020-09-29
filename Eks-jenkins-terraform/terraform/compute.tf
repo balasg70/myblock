4186ec5569821e3a11b13b9e8bed66f88c340c2e
balasg70 <56418977+balasg70@users.noreply.github.com>
2020-09-29 22:28:12 +0800

    Add files via upload

--- Eks-jenkins-terraform/terraform/compute.tf
+++ Eks-jenkins-terraform/terraform/compute.tf
@@ -17,14 +17,18 @@ data "aws_ami" "amazon-linux-2" {
 }
 
 resource "aws_instance" "jenkins-instance" {
-  ami             = "${data.aws_ami.amazon-linux-2.id}"
+#  ami             = "${data.aws_ami.amazon-linux-2.id}"
+  ami              = "amzn2-ami-hvm-2.0.20180622.1-x86_64-gp2"
   instance_type   = "t2.medium"
-  key_name        = "${var.keyname}"
+#  key_name        = "${var.keyname}"
+  key_name        = "aws_key_pair.deployer.key_name"
   #vpc_id          = "${aws_vpc.development-vpc.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
-  subnet_id          = "${aws_subnet.public-subnet-1.id}"
+#  subnet_id          = "${aws_subnet.public-subnet-1.id}"
+  subnet_id          = "aws_subnet.public-subnet-1.id"
   #name            = "${var.name}"
-  user_data = "${file("install_jenkins.sh")}"
+#  user_data = ${file("install_jenkins.sh")}
+  user_data = "data.template_file.user_data.rendered"
 
   associate_public_ip_address = true
   tags = {
@@ -35,7 +39,7 @@ resource "aws_instance" "jenkins-instance" {
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
-  vpc_id      = "${aws_vpc.development-vpc.id}"
+  vpc_id      = "aws_vpc.development-vpc.id"
 
   ingress {
     from_port   = 22
@@ -61,4 +65,18 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
 
 output "jenkins_ip_address" {
   value = "${aws_instance.jenkins-instance.public_dns}"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDF+n4+xsJLvFIRPi1EChe2JwoqNJM+qCjm8n2a8fxaIFWsRJSfpkN8DGDS9Mv+WCelcJ1tu8caClMj3q52emHDjsFjboSIiaS1PVJG3sTytCZhOPvtdSoN31ASlcR5ncdjGlF+1wvJWBO+9S6Yf71X0FKZxnHbB6hr0sfShckcGFcxEM42okhNmYa4XOfk8woJvSrpHjKPVfMVexWsMwdImLWCiNsUuYJsEfbotnWdXrBLs64GdFLk2mYTAF2XhabYiwWw5iJrb6KNWVf4hg2cLjAnTi8B0o/nkEhf2lIuN4XfEH59ZH8HylbZmo2GuWU4PpLcIxeszdv+f/DLJYUb Bala@Admins-MacBook-Pro.local"
+}
+
+data "template_file" "user_data" {
+  template = "${file("install_jenkins.sh.tpl")}"
+# {
+#   template = <<EOF
+#install_jenkins.sh
+#EOF
+#}
 }
